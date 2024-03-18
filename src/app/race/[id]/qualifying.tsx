e833875d7911e0b7c349bc309805294ce643b8e8
anilyavas <anil.yavas99@gmail.com>
2024-03-18 14:17:43 +0300

    qualifying results screen

--- src/app/race/[id]/qualifying.tsx
+++ src/app/race/[id]/qualifying.tsx
@@ -1,9 +1,37 @@
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, ActivityIndicator } from 'react-native';
 import raceRankingResponse from '../../../../assets/data/race-rankings.json';
 import RankingListItem from '../../../components/RankingListItem';
+import { useQuery, gql } from '@apollo/client';
+import { useGlobalSearchParams } from 'expo-router';
 
-const raceRankings = raceRankingResponse.data.raceRankings.response;
+const query = gql`
+  query MyQuery($id: String) {
+    raceRankings(race: $id) {
+      response {
+        grid
+        driver {
+          id
+          name
+          image
+        }
+        team {
+          id
+          name
+        }
+      }
+    }
+  }
+`;
 const QualifyingScreen = () => {
+  const { id } = useGlobalSearchParams();
+  const { data, loading } = useQuery(query, { variables: { id: String(id) } });
+
+  if (loading) {
+    return <ActivityIndicator />;
+  }
+
+  const raceRankings = data?.raceRankings?.response;
+
   return (
     <FlatList
       data={raceRankings}
