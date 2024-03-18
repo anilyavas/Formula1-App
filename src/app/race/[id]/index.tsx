bca1159e2e8c29fab52b5834c676ea8fdd0ae41e
anilyavas <anil.yavas99@gmail.com>
2024-03-18 17:31:25 +0300

    circuit info datas

--- src/app/race/[id]/index.tsx
+++ src/app/race/[id]/index.tsx
@@ -2,6 +2,7 @@ import { useLocalSearchParams } from 'expo-router';
 import { View, Text, StyleSheet, Image, ActivityIndicator } from 'react-native';
 import { Colors } from '../../../Constants/colors';
 import { useQuery, gql } from '@apollo/client';
+import RaceInformations from '../../../components/RaceInformations';
 
 const query = gql`
   query MyQuery($id: Int) {
@@ -24,6 +25,7 @@ const query = gql`
     }
   }
 `;
+
 const RaceDetails = () => {
   const { id } = useLocalSearchParams();
   const { data, loading, error } = useQuery(query, { variables: { id: id } });
@@ -50,42 +52,7 @@ const RaceDetails = () => {
           resizeMode='contain'
         />
       </View>
-      <View style={styles.raceDetails}>
-        <View style={styles.largeBox}>
-          <Text style={styles.title}>Circuit lenght</Text>
-          <Text style={styles.subtitle}>
-            4303
-            <Text style={{ fontSize: 16 }}> KM</Text>
-          </Text>
-        </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            margin: 10,
-            width: '100%',
-          }}
-        >
-          <View style={styles.smallBox}>
-            <Text style={styles.title}>No of laps</Text>
-            <Text style={styles.subtitle}>71</Text>
-          </View>
-          <View style={styles.smallBox}>
-            <Text style={styles.title}>First Grand Prix</Text>
-            <Text style={styles.subtitle}>1963</Text>
-          </View>
-        </View>
-        <View style={styles.largeBoxes}>
-          <Text style={styles.title}>Race distance</Text>
-          <Text style={styles.subtitle}>{race.distance}</Text>
-        </View>
-        <View style={styles.largeBoxes}>
-          <Text style={styles.title}>Lap record</Text>
-          <Text style={styles.subtitle}>1:17.774</Text>
-          <Text style={{ color: 'dimgrey', paddingTop: 5 }}>
-            Valtteri Bottas (2021)
-          </Text>
-        </View>
-      </View>
+      <RaceInformations />
     </View>
   );
 };
@@ -112,43 +79,5 @@ const styles = StyleSheet.create({
   circuit: {
     width: '100%',
     aspectRatio: 16 / 9,
-  },
-  raceDetails: {},
-  largeBox: {
-    margin: 10,
-    padding: 10,
-    borderTopColor: Colors.primary,
-    borderTopWidth: 5,
-    borderRadius: 10,
-    borderRightColor: Colors.primary,
-    borderRightWidth: 5,
-  },
-  smallBox: {
-    borderBottomColor: 'gainsboro',
-    borderBottomWidth: 3,
-    borderRightColor: 'gainsboro',
-    borderRightWidth: 3,
-    borderRadius: 10,
-    width: '48%',
-    padding: 10,
-  },
-  largeBoxes: {
-    margin: 10,
-    padding: 10,
-    borderBottomColor: 'gainsboro',
-    borderBottomWidth: 5,
-    borderRadius: 10,
-    borderRightColor: 'gainsboro',
-    borderRightWidth: 5,
-  },
-  title: {
-    color: 'grey',
-    fontFamily: 'F1-Regular',
-    fontSize: 14,
-    marginBottom: 5,
-  },
-  subtitle: {
-    fontFamily: 'F1-Bold',
-    fontSize: 25,
   },
 });
--- src/components/RaceInformations.tsx
+++ src/components/RaceInformations.tsx
@@ -1,11 +1,110 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
+import raceDetails from '../../assets/data/circuit.json';
+import { Colors } from '../Constants/colors';
+import { useLocalSearchParams } from 'expo-router';
+import { useQuery, gql } from '@apollo/client';
 
+const query = gql`
+  query MyQuery($id: Int!) {
+    circuit(id: $id) {
+      response {
+        first_grand_prix
+        race_distance
+        length
+        laps
+        lap_record {
+          driver
+          time
+          year
+        }
+      }
+    }
+  }
+`;
 const RaceInformations = () => {
+  const { id } = useLocalSearchParams();
+  const { data, loading } = useQuery(query, { variables: { id: id } });
+
+  if (loading) {
+    return <ActivityIndicator />;
+  }
+  const circuit = data?.circuit.response;
   return (
-    <View>
-      <Text>RaceInformations</Text>
+    <View style={styles.raceDetails}>
+      <View style={styles.largeBox}>
+        <Text style={styles.title}>Circuit lenght</Text>
+        <Text style={styles.subtitle}>{circuit.lenght}</Text>
+      </View>
+      <View
+        style={{
+          flexDirection: 'row',
+          margin: 10,
+          width: '100%',
+        }}
+      >
+        <View style={styles.smallBox}>
+          <Text style={styles.title}>No of laps</Text>
+          <Text style={styles.subtitle}>{circuit.laps}</Text>
+        </View>
+        <View style={styles.smallBox}>
+          <Text style={styles.title}>First Grand Prix</Text>
+          <Text style={styles.subtitle}>{circuit.first_grand_prix}</Text>
+        </View>
+      </View>
+      <View style={styles.largeBoxes}>
+        <Text style={styles.title}>Race distance</Text>
+        <Text style={styles.subtitle}>{circuit.race_distance}</Text>
+      </View>
+      <View style={styles.largeBoxes}>
+        <Text style={styles.title}>Lap record</Text>
+        <Text style={styles.subtitle}>{circuit.lap_record.time}</Text>
+        <Text style={{ color: 'dimgrey', paddingTop: 5 }}>
+          {circuit.lap_record.driver} {circuit.lap_record.year}
+        </Text>
+      </View>
     </View>
   );
 };
 
 export default RaceInformations;
+
+const styles = StyleSheet.create({
+  raceDetails: {},
+  largeBox: {
+    margin: 10,
+    padding: 10,
+    borderTopColor: Colors.primary,
+    borderTopWidth: 5,
+    borderRadius: 10,
+    borderRightColor: Colors.primary,
+    borderRightWidth: 5,
+  },
+  smallBox: {
+    borderBottomColor: 'gainsboro',
+    borderBottomWidth: 3,
+    borderRightColor: 'gainsboro',
+    borderRightWidth: 3,
+    borderRadius: 10,
+    width: '48%',
+    padding: 10,
+  },
+  largeBoxes: {
+    margin: 10,
+    padding: 10,
+    borderBottomColor: 'gainsboro',
+    borderBottomWidth: 5,
+    borderRadius: 10,
+    borderRightColor: 'gainsboro',
+    borderRightWidth: 5,
+  },
+  title: {
+    color: 'grey',
+    fontFamily: 'F1-Regular',
+    fontSize: 14,
+    marginBottom: 5,
+  },
+  subtitle: {
+    fontFamily: 'F1-Bold',
+    fontSize: 25,
+  },
+});
